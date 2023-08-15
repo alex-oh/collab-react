f3b31e62c84527376b8c8c199cd3c6168a4b5431
alex-oh <oh.alexj@gmail.com>
2023-08-15 16:06:14 -0400

    update app js structure

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import GlobalNav from './components/navigation/nav-bar';
-import ProjectForm from './pages/create-project';
-import APICards from './pages/api-finder';
+// library imports
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { HashRouter, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router";
+
+// component migration
+import GlobalNav from "./components/navigation/nav-bar";
+import ProjectForm from "./pages/create-project";
+import APICards from "./pages/api-finder";
+import Home from "./pages/home";
 
 function App() {
-  return (
-    <div className="App">
-
-
-      <GlobalNav /> 
-      <ProjectForm /> 
-      {/* <APICards/> */}
-
-
-    </div>
-  );
+    return (
+        <HashRouter>
+            <div className="App">
+                <GlobalNav />
+                <Routes>
+                    <Route path="/" element={<Navigate to="/home" />} />
+                    <Route path="/home" element={<Home />} />
+                </Routes>
+                <ProjectForm />
+                {/* <APICards/> */}
+            </div>
+        </HashRouter>
+    );
 }
-
 export default App;
