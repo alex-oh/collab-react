1b34fe1bd06a4950b72d847d2f26a615d2669722
alex-oh <oh.alexj@gmail.com>
2023-08-16 11:39:12 -0400

    update global css formatting

---
+++ src/components/fonts-test.js
@@ -0,0 +1,15 @@
+import React from "react";
+function FontsTest() {
+    return (
+        <div className="text-left pl-3">
+            <h1>This is H1</h1>
+            <h2>This is H2</h2>
+            <h3>This is H3</h3>
+            <h4>This is H4</h4>
+            This is standard body text<br/>
+            <em>This is italicized body text using em</em>
+            <p>This is paragraph text</p>
+        </div>
+    )
+}
+export default FontsTest;
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,10 +1,18 @@
 import React from "react";
+import './index.css';
 
 function Login() {
     return (
-        <>
-            <h1>Login</h1>
-        </>
+        <div>
+            <h3>Sign into Collab</h3>
+            <div class="login-box">
+                <p>Username</p>
+                <input></input>
+                <p>Password</p>
+                <input></input>
+                <button className="login-button"/>
+            </div>
+        </div>
     );
 }
 export default Login;
