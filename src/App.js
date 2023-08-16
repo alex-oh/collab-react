f5b373c17cc36b284e2c2e8c6935162bf56743d4
alex-oh <oh.alexj@gmail.com>
2023-08-16 11:38:50 -0400

    update app.js routes

--- src/App.js
+++ src/App.js
@@ -10,15 +10,21 @@ import GlobalNav from "./components/navigation/nav-bar";
 import ProjectForm from "./pages/create-project";
 import APICards from "./pages/api-finder";
 import Home from "./pages/home";
+import Login from "./pages/login";
+
+// temp components for dev reference
+import FontsTest from "./components/fonts-test";
 
 function App() {
     return (
         <HashRouter>
             <div className="App">
+                <FontsTest />
                 <GlobalNav />
                 <Routes>
                     <Route path="/" element={<Navigate to="/home" />} />
                     <Route path="/home" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
                 </Routes>
                 <ProjectForm />
                 {/* <APICards/> */}
