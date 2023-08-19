dfa109deaf1721934bf1ccf06bdacf3344a18cba
Krugertom <krugertom@outlook.com>
2023-08-18 20:06:00 -0400

    Updated API finder & Details

--- src/App.js
+++ src/App.js
@@ -4,15 +4,33 @@ import './App.css';
 import GlobalNav from './components/navigation/nav-bar';
 import ProjectForm from './pages/create-project';
 import APICards from './pages/api-finder';
+import ProjectDetails from './pages/project-details'
+import APIDetails from './pages/api-details'
 
 function App() {
+
+  const projectData = {
+    name: 'Sample Project',
+    createDate: '2023-08-01',
+    startDate: '2023-08-05',
+    owner: 'John Doe',
+    category: 'Web Development',
+    completion: 70,
+    description: 'This is a sample project for web development using React and Bootstrap.'
+  };
+
+
   return (
+    
     <div className="App">
 
-
+    
       <GlobalNav /> 
-      <ProjectForm /> 
+      {/* <ProjectForm />  */}
+      {/* <ProjectDetails/> */}
       {/* <APICards/> */}
+      <APIDetails/>
+
 
 
     </div>
---
+++ src/pages/api-details/index.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Badge from 'react-bootstrap/Badge';
+import Button from 'react-bootstrap/Button';
+import './index.css';
+import { useEffect } from 'react';
+import { useState } from 'react';
+
+
+
+// Test API data
+const apiTest = {
+    API: "Carbon Interface",
+    Description: "API to calculate carbon (C02) emissions estimates for common C02 emitting activities",
+    Auth: "apiKey",
+    HTTPS: true,
+    Cors: "yes",
+    Link: "https://docs.carboninterface.com/",
+    Category: "Environment",
+    favoriteUsers: ["Tom", "Alex", "Frank"]
+};
+
+
+function APIDetails({ api = apiTest }) { // Set default prop value to the testAPI
+
+    const [userDetails, setUserDetails] = useState([]);
+
+    useEffect(() => {
+        // Fetch user details for each Object ID in the favoriteUsers array
+        Promise.all(api.favoriteUsers.map(uid =>
+            fetch(`/api/users/${uid}`)
+                .then(response => response.json())
+        ))
+            .then(users => setUserDetails(users))
+            .catch(error => console.error("Error fetching user details:", error));
+    }, [api.favoriteUsers]);
+
+    return (
+        <div className="project-details-container">
+            <Card className="dark-card">
+                <Card.Header as="h5">{api.API} </Card.Header>
+
+                <Card.Body className="dark-card-body">
+                    <Badge className="category-badge">{api.Category}</Badge>
+                    <Card.Text style={{ fontStyle: 'italic' }}>
+                        Supports CORS? <h4>{api.Cors}</h4>
+                    </Card.Text>
+
+                    <Card.Text>
+                        AUTH Method: <h4>{api.Auth}</h4>
+                    </Card.Text>
+                    <Card.Text>
+                        HTTPS? <h4>{api.HTTPS ? "Yes" : "No"}</h4>
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+
+            <Card className="dark-card">
+                <Card.Header as="h5">Description</Card.Header>
+                <Card.Body className="dark-card-body">
+                    <Card.Text>{api.Description}</Card.Text>
+                </Card.Body>
+            </Card>
+
+            <Card className="dark-card">
+                <Card.Header as="h5">These users have also favorited this API:</Card.Header>
+                <Card.Body className="dark-card-body">
+                    {userDetails.map(user => (
+                        <Badge
+                            key={user._id} // Assuming each user object has an _id property 
+                            style={{
+                                backgroundColor: 'blue',
+                                color: 'white',
+                                marginRight: '5px',
+                                fontSize: '1.2em',
+                                padding: '5px 10px',
+                                cursor: 'pointer'
+                            }}
+                            onClick={() => window.location.href = `/api/users/${user._id}`}
+                        >
+                            {user.username} // Assuming each user object has a username property
+                        </Badge>
+                    ))}
+                </Card.Body>
+            </Card>
+        </div>
+    );
+}
+
+export default APIDetails;
