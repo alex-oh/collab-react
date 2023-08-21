179ff8088d91a0417380cedae3333163b87b2742
alex-oh <oh.alexj@gmail.com>
2023-08-21 14:18:30 -0400

    refactor user profile card to query server for user projects

--- src/components/edit-profile-projects/edit-profile-project.js
+++ src/components/edit-profile-projects/edit-profile-project.js
@@ -1,56 +1,42 @@
 import React, { useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
+// import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 
 import ProjectSummaryCard from "../../components/project-summary-card";
 
-import { findMyProjectsThunk, findProjectsThunk } from "../../redux-services/projects/projects-thunks";
-import { findUserThunk } from "../../redux-services/users/user-thunks";
+import { findMyProjects } from "../../redux-services/projects/projects-service";
+import { findUser } from "../../redux-services/users/users-service";
 
 function EditProfileProject() {
     const params = useParams();
-    const dispatch = useDispatch();
+    const userId = params.uid;
 
-    // Ensure projects defaults to an empty array if undefined
-    const projects = useSelector((state) => state.projects.myProjects) || []; 
-
+    const [projects, setProjects] = useState([]);
     const [user, setUser] = useState([]);
 
-    let userId = null;
-
-    if(!params.uid) {
-        console.log("current user's page");
-    } else {
-        userId = params.uid;
-    }
-
+    // loading functions
     const loadUser = async () => {
-        const userToLoad = await dispatch(findUserThunk(userId));
+        const userToLoad = await findUser(userId);
         setUser(userToLoad);
     };
+    const loadMyProjects = async () => {
+        const response = await findMyProjects(user);
+        setProjects(response);
+    };
 
+    // load user and projects
     useEffect(() => {
         loadUser();
-        if (userId) {
-            dispatch(findMyProjectsThunk(user));
-        }
+        loadMyProjects();
     }, [user]);
 
     return (
         <div>
-            
             <div className="mt-4 col-xs-7 col-sm-7 col-md-7 col-lg-7 border border-secondary text-left main-content">
                 <h4></h4>
-                <div className="row Active_Project_Container">
-                    {projects.map((project) => (
-                        <div key={project._id} className="Project_Name_1">
-                            <div className="col-4 mt-2 py-2 px-2 w-75 Active_Project_Container_image">
-                                <ProjectSummaryCard project={project} />
-                            </div>
-                        </div>
-                    ))}
-                </div>
-                
+                {projects.map((project) => (
+                    <ProjectSummaryCard project={project} />
+                ))}
             </div>
         </div>
     );
